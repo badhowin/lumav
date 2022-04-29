86e0d487851e3fabcffa1db4dddb04b74287495d
Maksim Pearnberg <maskcool@mail.ru>
2022-04-29 16:25:24 +0300

    start project

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+
+import './app.css';
+
+export default class App extends Component {
+
+  render(){
+      return  (
+            <div>
+              Lumav
+            </div>
+          );
+      }
+};
