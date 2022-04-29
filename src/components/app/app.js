fc119e609145fd211e5cb48a619c191d74cd3278
Maksim Pearnberg <maskcool@mail.ru>
2022-04-29 23:05:39 +0300

    css first ver ready

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,9 +16,14 @@ export default class App extends Component {
                     </div>
                     <div className = "form-part box-part">
                         <div className = "add-product-form">
-                            <div className = "form-file-input"></div>
-                            <input className = "form-name-field"></input>
-                            <input className = "form-price-field"></input>
+                            <form>
+                                <div className = "form-file-input form-input"></div>
+                                <input className = "form-name-field form-input"></input>
+                                <input className = "form-price-field form-input"></input>
+                                <div className="button-wrapper">
+                                    <button className = "form-button-submit">Submit</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                     <div className = "cart-part box-part">
@@ -26,6 +31,70 @@ export default class App extends Component {
                     </div>
                 </div>
                 <div className = "product-grid">
+
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
+                    <div className = "product-card">
+                        <div className = "delete-button-wrap">
+                            <div className = "delete-button">X</div>
+                        </div>
+                        <img className = "product-card-img"></img>
+                        <div className = "product-name">Product Name</div>
+                        <div className = "product-price">Product Price</div>
+                        <button className = "btn-add-to-cart">Add to cart</button>
+                    </div>
                     <div className = "product-card">
                         <div className = "delete-button-wrap">
                             <div className = "delete-button">X</div>
